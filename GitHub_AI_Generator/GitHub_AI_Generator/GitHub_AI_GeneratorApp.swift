a556af60c31baaf8694464a4867062e215fbbcd8
Viemem <lcmem08@gmail.com>
2024-03-13 19:39:18 -0600

    First

---
+++ GitHub_AI_Generator/GitHub_AI_Generator/GitHub_AI_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_AI_GeneratorApp.swift
+//  GitHub_AI_Generator
+//
+//  Created by Guillermo Emiliano Landa Cerpa on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_AI_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
